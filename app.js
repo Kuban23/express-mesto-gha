ac25835df89c3684b2d9c9ccddc79b8ebb23869e
Esin Andrew <kubanesin@gmail.com>
2022-04-11 22:47:38 +0300

    feat: express, mongo

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// Слушаем 3000 порт
+const { PORT = 3000 } = process.env;
+
+// подключаемся к серверу mongo
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});

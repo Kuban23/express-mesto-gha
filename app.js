58f72a6a42eb1952c798adf958d7282498b23ab2
Esin Andrew <kubanesin@gmail.com>
2022-04-11 23:33:24 +0300

    feat:Создал схемы и модели user и card

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -7,7 +7,7 @@ const app = express();
 const { PORT = 3000 } = process.env;
 
 // подключаемся к серверу mongo
-mongoose.connect('mongodb://localhost:27017/mestodb', {
+mongoose.connect("mongodb://localhost:27017/mestodb", {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,

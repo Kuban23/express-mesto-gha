bd17867e6701b07dd154bd0520de367d00d74259
Esin Andrew <kubanesin@gmail.com>
2022-04-13 14:09:23 +0300

    feat:добавил  контроллеры для карточек like dislike

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,6 +1,8 @@
 // GET /cards — возвращает все карточки
 // POST /cards — создаёт карточку
 // DELETE /cards/:cardId — удаляет карточку по идентификатору
+// PUT /cards/:cardId/likes — поставить лайк карточке
+// DELETE /cards/:cardId/likes — убрать лайк с карточки
 
 const Card = require("../models/card");
 
@@ -34,4 +36,30 @@ module.exports.removeCard = (req, res) => {
   Card.findByIdAndRemove(req.params.cardId)
     .then((card) => res.send({ data: card }))
     .catch(() => res.status(500).send({ message: "Ошибка при удалении карточки" }));
+};
+
+// Ставим лайк карточке
+module.exports.likeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
+    { new: true },
+  )
+    .then((card) => {
+      res.status(200).send({ data: card });
+    })
+    .catch(() => res.status(500).send({ message: "Ошибка при постанке лайка" }));
+};
+
+// Удаляем лайк с карточки
+module.exports.dislikeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } }, // убрать _id из массива
+    { new: true },
+  )
+    .then((card) => {
+      res.status(200).send({ data: card });
+    })
+    .catch(() => res.status(500).send({ message: "Ошибка при удалении лайка" }));
 };

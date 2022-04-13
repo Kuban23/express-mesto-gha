943f2d8ce53aeadf391ea72ee99027bdc064955a
Esin Andrew <kubanesin@gmail.com>
2022-04-13 14:14:18 +0300

    feat:добавил роуты для карточек like dislike

--- routes/cards.js
+++ routes/cards.js
@@ -3,12 +3,18 @@
 // GET /cards — возвращает все карточки
 // POST /cards — создаёт карточку
 // DELETE /cards/:cardId — удаляет карточку по идентификатору
+// PUT /cards/:cardId/likes — поставить лайк карточке
+// DELETE /cards/:cardId/likes — убрать лайк с карточки
 
 const router = require("express").Router();
-const { getCards, createCard, removeCard } = require("../controllers/cards");
+const {
+  getCards, createCard, removeCard, likeCard, dislikeCard,
+} = require("../controllers/cards");
 
 router.get("/", getCards);
 router.post("/", createCard);
 router.delete("/:cardId", removeCard);
+router.put("/cards/:cardId/likes", likeCard);
+router.delete("/cards/:cardId/likes", dislikeCard);
 
 module.exports = router;
